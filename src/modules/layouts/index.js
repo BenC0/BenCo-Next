ea0f7101dc3f497efff66afea55e5ba9bf08e931
Benji Cohen <suddensquirrel94@gmail.com>
2023-05-08 15:53:20 +0100

    4th pass at FOOC bug...

--- src/modules/layouts/index.js
+++ src/modules/layouts/index.js
@@ -3,8 +3,8 @@ import { MenuIcon, Menu } from "../Menu"
 export default function Layout({ children, page }) {
 	return (
 		<main id="root" className="m-grid-4 d-grid-12" page={ page } >
-			{children}
 			<Menu />
+			{children}
 			<MenuIcon />
 		</main>
 	)
